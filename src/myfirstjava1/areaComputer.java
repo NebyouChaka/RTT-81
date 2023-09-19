7ab339762e1945ac6558fb6b190e2fe905de06c2
Nebyou Chaka <nebyouchaka@gmail.com>
2023-09-19 14:39:22 -0400

    this is the very first time commit

--- src/myfirstjava1/areaComputer.java
+++ src/myfirstjava1/areaComputer.java
@@ -1,10 +1,20 @@
 package myfirstjava1;
 
-public class areaComputer {
+public static void main(String[] args) {
+    double radius;
+    double area;
+    
+    // Assign a radius
+    radius = 20;
+    
+    // Compute area
+    area = radius * radius * 3.14159;
+    
+    // Display results
+    System.out.println("The area for the circle of radius " +
+      radius + " is " + area);
+  }
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
