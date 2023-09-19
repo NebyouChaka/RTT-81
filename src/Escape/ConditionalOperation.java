5466878bc0936e3fbc96e41a5395e5707fe6bb52
Nebyou Chaka <nebyouchaka@gmail.com>
2023-09-19 15:49:17 -0400

    adding new staff

--- src/Escape/ConditionalOperation.java
+++ src/Escape/ConditionalOperation.java
@@ -4,27 +4,31 @@ public class ConditionalOperation {
 	public static void main(String[] args) {
 		boolean conditionOne = false;
 		boolean conditionTwo = false;
-		//&& is and
-		if(conditionOne && conditionTwo) {
-			//do something only if both variable are true
+		// && is and
+		if (conditionOne && conditionTwo) {
+			// do something only if both variable are true
 			System.out.println("Both variable are true");
 		}
-		//|| is or
-		if(conditionOne || conditionTwo ) {
-			//do something if one or the other or both variable are true
+		// || is or
+		if (conditionOne || conditionTwo) {
+			// do something if one or the other or both variable are true
 			System.out.println("Either or both of the variable is true");
 		}
-		//this is using mathmatical operation
+		//neither is true
+		if (!conditionOne && !conditionTwo) {
+			System.out.println("Neither is True");
+		}
+		// this is using mathmatical operation
 		int x = 10;
-		if ((x > 3) && ( x < 20)) {
+		if ((x > 3) && (x < 20)) {
 			System.out.println("x is between 3 and 20");
 		}
-		//odd vs even
+		// odd vs even
 		int y = 4;
-		if ( (y % 2) == 1) {
+		if ((y % 2) == 1) {
 			System.out.println("This is odd number");
 		} else {
 			System.out.println("This is even number");
 		}
-}
+	}
 }
