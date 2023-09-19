750b1756527dc99fa5f6e98dc5fd068265149ea9
Nebyou Chaka <nebyouchaka@gmail.com>
2023-09-19 15:01:21 -0400

    second commit

---
+++ src/Escape/charachters1.java
@@ -0,0 +1,37 @@
+package Escape;
+
+public class charachters1 {
+
+		public static void main(String[] args) {
+
+			// the main ones to know are ... \t, \n, and \"
+
+
+			// for the language java \ (slash) is the escape character
+
+			// this is an example of the tab escape character \t
+			System.out.println("This is a tab character ->\t<-");
+
+			// basic formating with tabs
+			System.out.println("1\tOne\t\tCol3");
+			System.out.println("10\tTen\t\tCol3");
+			//this one is long so it only needs 1 tab for the 3rd column
+			System.out.println("100\tOne Hundred\tCol3"); 
+
+			// this is an example of a new line character \n
+			System.out.println("This is line 1\nThis is line 2");
+			System.out.println("This is line 3");
+			System.out.println("This is line 4");
+
+
+			// what happens if I want to print just a \
+			System.out.println("To get a slash to print we need 2 of them \\");
+
+			// what happens if I want a " in my string
+			System.out.println("This is a quote \" in a string ");
+					
+		}
+
+	}
+
+
---
+++ src/Escape/variable2.java
@@ -0,0 +1,19 @@
+package Escape;
+
+public class variable2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			String x = new String();
+			x.toLowerCase();
+			x.toUpperCase();
+
+			int j = 4;
+
+			Integer k = Integer.valueOf(5);
+
+
+			boolean tuesday = true;
+			Boolean wednesday = new Boolean(false);
+	}
+}
